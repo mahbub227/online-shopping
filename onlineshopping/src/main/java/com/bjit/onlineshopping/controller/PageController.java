3c19ff952713ceecb1fc9e51114f7a7bdb54e996
Mahbub Ul Alam <m.alam@bjitgroup.com>
2018-02-02 18:19:51 +0600

    Successfully loaded! Home from Change-Id: Ia6ba7044976722cd695803ef20cb2ded65f5d637 Signed-off-by: shamsul <shamsul@Rumman>

--- onlineshopping/src/main/java/com/bjit/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/bjit/onlineshopping/controller/PageController.java
@@ -8,11 +8,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-
 	@RequestMapping(value= {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv= new ModelAndView("page");
-		mv.addObject("greeting","welcome to spring mvc");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
+		return mv;
+	}
+	
+	@RequestMapping(value= "/about")
+	public ModelAndView about() {
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;
+	}
+
+	@RequestMapping(value= "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("title","Contact");
+		mv.addObject("userClickContact",true);
 		return mv;
 	}
 	
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/about.jsp
@@ -0,0 +1,5 @@
+<div class="container">
+<div class="row">
+About us page
+</div>
+</div>
