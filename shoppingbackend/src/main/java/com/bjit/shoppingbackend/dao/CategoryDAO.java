42ab2f9ab59aa82c81c22d180ba7c0b8946cd2d8
shamsul <shamsul@Rumman>
2018-02-05 16:43:59 +0600

    hibernate done!

--- shoppingbackend/src/main/java/com/bjit/shoppingbackend/dao/CategoryDAO.java
+++ shoppingbackend/src/main/java/com/bjit/shoppingbackend/dao/CategoryDAO.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import com.bjit.shoppingbackend.dto.Category;
 
+
 public interface CategoryDAO {
+
 	
-	boolean add(Category category);
-	
-	List<Category>list();
 	
 	Category get(int id);
-
+	List<Category> list();
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
+	
+	
 }
--- shoppingbackend/src/main/java/com/bjit/shoppingbackend/daoimpl/CategoryDAOImpl.java
+++ shoppingbackend/src/main/java/com/bjit/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -1,9 +1,9 @@
 package com.bjit.shoppingbackend.daoimpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,62 +12,78 @@ import com.bjit.shoppingbackend.dao.CategoryDAO;
 import com.bjit.shoppingbackend.dto.Category;
 
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDAOImpl implements CategoryDAO {
-	
+
 	@Autowired
 	private SessionFactory sessionFactory;
-	
-	private static List<Category>categories=new ArrayList<>();
-	
-	static {
-		Category category=new Category();
-		category.setId(1);
-		category.setName("Television");
-		category.setDescription("Watch TV");
-		category.setImageURL("image.png");
-		categories.add(category);
-		Category category2=new Category();
-		category2.setId(2);
-		category2.setName("Laptop");
-		category2.setDescription("All Kinds of");
-		category2.setImageURL("image1.png");
-		categories.add(category2);
-		Category category3=new Category();
-		category3.setId(3);
-		category3.setName("Mobile");
-		category3.setDescription("Call friend");
-		category3.setImageURL("image3.png");
-		categories.add(category3);
-	}
 
 	@Override
 	public List<Category> list() {
-		// TODO Auto-generated method stub
-		return categories;
+		
+		String selectActiveCategory = "FROM Category WHERE active = :active";
+		
+		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
+				
+		query.setParameter("active", true);
+						
+		return query.getResultList();
+	}
+
+	/*
+	 * Getting single category based on id
+	 */
+	@Override
+	public Category get(int id) {
+
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
+
 	}
 
 	@Override
-	public Category get(int id) {
-		// TODO Auto-generated method stub
-		for(Category category : categories) {
-			if(category.getId()==id) return category;
-		}
-		return null;
-	}
-	
-	
-	@Transactional
+
 	public boolean add(Category category) {
-		// TODO Auto-generated method stub
+
 		try {
+			// add the category to the database table
 			sessionFactory.getCurrentSession().persist(category);
 			return true;
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return false;
 		}
+
+	}
+
+	/*
+	 * Updating a single category
+	 */
+	@Override
+	public boolean update(Category category) {
+
+		try {
+			// add the category to the database table
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(Category category) {
 		
+		category.setActive(false);
+		
+		try {
+			// add the category to the database table
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
 	}
 
 }
