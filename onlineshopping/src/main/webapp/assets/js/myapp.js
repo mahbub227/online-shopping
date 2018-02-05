22e020380e3a04c296c89d14919b944c1ea11e94
Mahbub Ul Alam <m.alam@bjitgroup.com>
2018-02-05 15:14:49 +0600

    commit : 03-06 before!

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -1,12 +1,13 @@
 $(function(){
 	switch(menu){
+	case 'Home': $('#home').addClass('active');
+	break;
 	case 'About Us': $('#about').addClass('active');
 	break;
 	case 'Contact': $('#contact').addClass('active');
 	break;
 	case 'All Products' : $('#listProducts').addClass('active');
 	break;
-	
 	default: $('#listProducts').addClass('active');
 			 $('#a_'+menu).addClass('active');
 	break;
