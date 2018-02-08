a144e8e86d3f9a47dbaad50796324baa17ea81f1
shamsul <shamsul@Rumman>
2018-02-08 16:29:37 +0600

    from home 10:30 8th feb 2018

---
+++ onlineshopping/src/main/java/com/bjit/onlineshopping/exception/GlobalDefaultExceptionController.java
@@ -0,0 +1,20 @@
+package com.bjit.onlineshopping.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalDefaultExceptionController {
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ModelAndView handlerNoHandlerFoundException() {
+		
+		ModelAndView mv = new ModelAndView("error");
+		mv.addObject("errorTitle","Page Request is Invalid!");
+		mv.addObject("errorDescription","Sorry, We'll construct it soon!");
+		mv.addObject("title","404 Error Page");
+		return mv;
+	}
+}
